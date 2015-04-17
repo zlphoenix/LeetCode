f5dc9dbdbefde6bed45134c835b7a13fb5ace072
Allen Zhou <zlphoenix@gmail.com>
2015-04-17 21:09:19 +0800

    ok

--- easy/src/test/java/com/allen/leecode/easy/easy.java
+++
@@ -1,9 +0,0 @@
-package com.allen.leecode.easy;
-
-/**
- * <p>项目名称:leecode </p>
- * <p>Copyright (c) 2015, Inspur GSP All Rights Reserved.</p>
- * <p>修改记录1: 新建文件-15/4/16上午12:56-zhoulun</p>
- */
-public class easy {
-}
